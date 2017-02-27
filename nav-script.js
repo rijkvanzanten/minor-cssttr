35f81e6eeb0a77eb3b51730b122823fa850ca954
Rijk van Zanten <rijkvanzanten@me.com>
2017-02-27 13:52:08 +0100

    :truck: split script file in two and start work on modal

--- nav-script.js
+++ nav-script.js
@@ -1,24 +1,24 @@
 /* global document */
 
-/**
- * Modeled after Terrill Thompson's example in jQuery
- * http: //staff.washington.edu/tft/tests/menus/simplyaccessible/index.html
- *
- * et all aria labels from JavaScript because in a real-world-scenario, the
- *  listitems are unknown and rendered dynamically
- */
-
 (function() {
+  /**
+   * Menu modeled after Terrill Thompson's example in jQuery
+   * http: //staff.washington.edu/tft/tests/menus/simplyaccessible/index.html
+   *
+   * Set all aria labels from JavaScript because in a real-world-scenario, the
+   *  listitems are unknown and rendered dynamically
+   */
+
   const config = {
     activeClass: 'show-menu',
   };
 
-  const keyCodeMap = {
-    48: '0', 49: '1', 50: '2', 51: '3', 52: '4', 53: '5', 54: '6', 55: '7', 56: '8', 57: '9', 59: ';',
-    65: 'a', 66: 'b', 67: 'c', 68: 'd', 69: 'e', 70: 'f', 71: 'g', 72: 'h', 73: 'i', 74: 'j', 75: 'k', 76: 'l',
-    77: 'm', 78: 'n', 79: 'o', 80: 'p', 81: 'q', 82: 'r', 83: 's', 84: 't', 85: 'u', 86: 'v', 87: 'w', 88: 'x', 89: 'y', 90: 'z',
-    96: '0', 97: '1', 98: '2', 99: '3', 100: '4', 101: '5', 102: '6', 103: '7', 104: '8', 105: '9'
-  };
+  // const keyCodeMap = {
+  //   48: '0', 49: '1', 50: '2', 51: '3', 52: '4', 53: '5', 54: '6', 55: '7', 56: '8', 57: '9', 59: ';',
+  //   65: 'a', 66: 'b', 67: 'c', 68: 'd', 69: 'e', 70: 'f', 71: 'g', 72: 'h', 73: 'i', 74: 'j', 75: 'k', 76: 'l',
+  //   77: 'm', 78: 'n', 79: 'o', 80: 'p', 81: 'q', 82: 'r', 83: 's', 84: 't', 85: 'u', 86: 'v', 87: 'w', 88: 'x', 89: 'y', 90: 'z',
+  //   96: '0', 97: '1', 98: '2', 99: '3', 100: '4', 101: '5', 102: '6', 103: '7', 104: '8', 105: '9'
+  // };
 
   // Add ARIA role to menubar and menuitems
   document.querySelectorAll('[aria-role="menubar"] li').forEach(listItem => listItem.setAttribute('role', 'menuitem'));
@@ -67,4 +67,5 @@
       activeMenu.querySelectorAll('a').forEach(link => link.setAttribute('tabindex', -1));
     });
   }
+
 })();
